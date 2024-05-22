ecf9eb4d7972bed2eac7cc6ab71ca2c1de24b5c7
Cognosonline <soporte@cognosonline.com>
2024-05-22 14:16:54 -0500

    prueba1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 const App = () => {
+
+  const [name, setName] = useState();
+
+  const handleName = async (e) => {
+    e.preventDefault();
+
+    try {
+      const result = await axios.get('https://gr61110iti.execute-api.us-east-2.amazonaws.com/dev/prueba')
+      setName(result.data.name)
+    } catch (error) {
+      console.log(error)
+    }
+    
+  }
+
   return (
     <div>
-      <h1>Hello, Mao!</h1>
+      <h1>Hola, {name}</h1>
+      <input placeholder='Digita tu nombre'/>
+      <button onClick={handleName}>Enviar</button>
     </div>
   );
 };
